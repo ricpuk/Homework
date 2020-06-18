8d235187914d8b74746098ff7150d35baf85d810
Ricardas Pukelis <ricardas.pukelis@gmail.com>
2020-06-18 22:50:07 +0300

    Add Configuration files demo added to program.cs

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -8,6 +8,8 @@ namespace Homework
         static void Main(string[] args)
         {
             var configurationService = new ConfigurationService();
+            configurationService.AddConfigurationFile("Base_Config.txt");
+            configurationService.AddConfigurationFile("Project_Config.txt");
             var config = configurationService.Configure<CustomConfig>();
         }
     }
