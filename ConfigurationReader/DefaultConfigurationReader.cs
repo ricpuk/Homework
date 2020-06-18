9f85ac111aa9dcfdc82072a39ce7a5ead1702939
Ricardas Pukelis <ricardas.pukelis@gmail.com>
2020-06-18 21:30:46 +0300

    Initial DefaultConfiguartionReader implementation

--- ConfigurationReader/DefaultConfigurationReader.cs
+++ ConfigurationReader/DefaultConfigurationReader.cs
@@ -1,13 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using ConfigurationReader.Interfaces;
 
 namespace ConfigurationReader
 {
     public class DefaultConfigurationReader : IConfigurationReader
     {
+        private readonly string _configDirectory = $"{Environment.CurrentDirectory}/Config";
+
         public List<ConfigurationEntry> ReadFromFile(string file)
         {
-            return new List<ConfigurationEntry>();
+            var filePath = $"{_configDirectory}/{file}";
+            var configurationLines = File.ReadAllLines(filePath)
+                .Where(x => !x.StartsWith('-') &&
+                            !x.StartsWith("===") &&
+                            !string.IsNullOrWhiteSpace(x) &&
+                            x.Contains(":\t"))
+                .ToList();
+            var configurationEntries = new List<ConfigurationEntry>();
+            foreach (var line in configurationLines)
+            {
+                var parts = line.Split(":\t");
+                if (parts.Length < 2)
+                {
+                    continue;
+                }
+                configurationEntries.Add(new ConfigurationEntry
+                {
+                    Key = parts[0].TrimEnd('\t'), 
+                    Value = parts[1].Split('\t', StringSplitOptions.RemoveEmptyEntries)[0].Trim('\t')
+                });
+
+            }
+
+            return configurationEntries;
         }
     }
 }
